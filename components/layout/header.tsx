c1906bb79d7a6b067f048818233ef664d728402a
Samuel <samuelaragon616@gmail.com>
2025-09-25 21:09:04 -0300

    update

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -41,7 +41,7 @@ export function Header() {
   }
 
   return (
-    <header className="sticky top-0 z-50 flex h-16 shrink-0 items-center gap-2 border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 px-4">
+    <header className="sticky top-0 z-50 flex h-16 shrink-0 items-center gap-2 border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 px-4 ">
       <SidebarTrigger className="-ml-1 md:hidden" />
 
       <div className="flex flex-1 items-center gap-2 px-3">
